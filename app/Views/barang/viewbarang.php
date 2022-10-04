b07e896ad21406c041936546bed0ac4b591c9b0f
adeputram <95565811+adeputram@users.noreply.github.com>
2022-10-04 10:48:30 +0700

    Pertemuan 1

---
+++ app/Views/barang/viewbarang.php
@@ -0,0 +1,13 @@
+<?= $this->extend('main/layout') ?>
+
+<?= $this->section('judul') ?>
+Ini Adalah Judul Barang
+<?= $this->endSection('judul') ?>
+
+<?= $this->section('subjudul') ?>
+Ini adalah Data Barang
+<?= $this->endSection('subjudul') ?>
+
+<?= $this->section('isi') ?>
+Ini adalah isi Barang
+<?= $this->endSection('isi') ?>
---
+++ app/Views/kategori/viewkategori.php
@@ -0,0 +1,13 @@
+<?= $this->extend('main/layout') ?>
+
+<?= $this->section('judul') ?>
+Ini Adalah Judul Kategori
+<?= $this->endSection('judul') ?>
+
+<?= $this->section('subjudul') ?>
+Ini adalah Data Kategori
+<?= $this->endSection('subjudul') ?>
+
+<?= $this->section('isi') ?>
+Ini adalah isi Kategori
+<?= $this->endSection('isi') ?>
