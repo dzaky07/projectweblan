937683fab655cfd4321a101322be82908fe6ee37
AkmalGilangRosadi <83489612+AkmalGilangRosadi@users.noreply.github.com>
2022-10-10 22:04:49 +0700

    Delete
    Bismillah

--- app/Controllers/Kategori.php
+++ app/Controllers/Kategori.php
@@ -3,11 +3,126 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\Modelkategori;
 
 class Kategori extends BaseController
 {
+    public function __construct()
+    {
+        $this->kategori = new Modelkategori();
+    }
     public function index()
     {
-        return view('kategori/viewkategori');
+        $data = [
+            'tampildata' => $this->kategori->findAll()
+        ];
+        return view('kategori/viewkategori', $data);
     }
-}
+
+    public function formtambah()
+    {
+        return view('kategori/formtambah');
+    }
+
+    public function simpandata()
+    {
+        $namakategori = $this->request->getVar('namakategori');
+
+        $validation = \config\Services::validation();
+
+        $valid = $this->validate([
+            'namakategori' => [
+                'rules' => 'required',
+                'label' => 'Nama Kategori',
+                'errors' => [
+                    'required' => '{field} tidak boleh kosong'
+                ]
+            ]
+        ]);
+
+        if (!$valid) {
+            $pesan = [
+                'errorNamaKategori' => '<br><div class="alert alert-danger">' . $validation->getError() . '</div>'
+            ];
+
+            session()->setFlashdata($pesan);
+            return redirect()->to('/kategori/formtambah');
+        }else{
+            $this->kategori->insert([
+                'katnama' => $namakategori
+            ]);
+
+            $pesan = [
+                'sukses' => '<div class="alert alert-success">Data Berhasil ditambahkan...</div>'
+            ];
+
+            session()->setFlashdata($pesan);
+            return redirect()->to('/kategori/index');
+        }
+    }
+    public function formedit($id){
+        $rowData = $this->kategori->find($id);
+        if($rowData){
+
+            $data =[
+                'id' => $id,
+                'nama' => $rowData['katnama']
+            ];
+
+            return view('kategori/formedit', $data);
+
+        }else{
+            exit('Data tidak ditemukan');
+        }
+    }
+    public function updatedata(){
+        $idkategori = $this->request->getVar('idkategori');
+        $namakategori = $this->request->getVar('namakategori');
+
+        $validation = \config\Services::validation();
+
+        $valid = $this->validate([
+            'namakategori' => [
+                'rules' => 'required',
+                'label' => 'Nama Kategori',
+                'errors' => [
+                    'required' => '{field} tidak boleh kosong'
+                ]
+            ]
+        ]);
+
+        if (!$valid) {
+            $pesan = [
+                'errorNamaKategori' => '<br><div class="alert alert-danger">' . $validation->getError() . '</div>'
+            ];
+
+            session()->setFlashdata($pesan);
+            return redirect()->to('/kategori/formedit/' .$idkategori);
+        }else{
+            $this->kategori->update($idkategori,[
+                'katnama' => $namakategori
+            ]);
+
+            $pesan = [
+                'sukses' => '<div class="alert alert-success">Data Berhasil diedit...</div>'
+            ];
+
+            session()->setFlashdata($pesan);
+            return redirect()->to('/kategori/index');
+        }
+    }
+    public function hapus($id){
+        $rowData = $this->kategori->find($id);
+        if($rowData){
+            $pesan = [
+                'sukses' => '<div class="alert alert-success">Data Berhasil dihapus...</div>'
+            ];
+
+            $this->kategori->delete($id);
+            
+
+        }else{
+            exit('Data tidak ditemukan');
+        }
+    }
+} 
