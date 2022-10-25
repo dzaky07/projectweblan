4f4b0ba563d69b04c7d0f2fbcf64009c3ecb7ea4
adeputram <95565811+adeputram@users.noreply.github.com>
2022-10-25 16:57:07 +0700

    Paging data barang
    Membuat Paging di Data Barang

--- app/Views/barang/viewbarang.php
+++ app/Views/barang/viewbarang.php
@@ -9,5 +9,82 @@ Ini adalah Data Barang
 <?= $this->endSection('subjudul') ?>
 
 <?= $this->section('isi') ?>
+<<<<<<< Updated upstream
 Ini adalah isi Barang
+=======
+<?= session()->getFlashdata('error'); ?>
+<?= session()->getFlashdata('sukses'); ?>
+<?= form_open('barang/index') ?>
+<div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="Cari data berdasarkan Kode, Nama Barang & Kategori" name="cari" autofocus>
+    <div class="input-group-append">
+        <button class="btn btn-outline-success" type="submit" name="tombolcari">
+            <i class="fa fa-search"></i>
+        </button>
+    </div>
+</div>
+<?= form_close(); ?>
+<table class="table table-striped table-bordered" style="width:100%;">
+    <thead>
+        <tr>
+            <th style="width: 5;">No</th>
+            <th>Kode Barang</th>
+            <th>Nama Barang</th>
+            <th>Kategori</th>
+            <th>Harga</th>
+            <th>Stok</th>
+            <th style="width: 15%;">Aksi</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php
+        $nomor = 1;
+        foreach ($tampildata as $row) :
+        ?>
+            <tr>
+                <td><?= $nomor++; ?></td>
+                <td><?= $row['brgkode']; ?></td>
+                <td><?= $row['brgnama']; ?></td>
+                <td><?= $row['katnama']; ?></td>
+                <td><?= number_format($row['brgharga'], 0); ?></td>
+                <td><?= number_format($row['brgstok'], 0); ?></td>
+                <td>
+
+                    <button type="button" class="btn btn-sm btn-info" onclick="edit('<?= $row['brgkode'] ?>')">
+                        <i class="fa fa-edit"></i>
+                    </button>
+
+                    <form method="POST" action="/barang/hapus/<?= $row['brgkode'] ?>" style="display:inline;" onsubmit="return hapus();">
+                        <input type="hidden" value="DELETE" name="_method">
+
+                        <button type="submit" class="btn btn-sm btn-danger" title="Hapus Data">
+                            <i class="fa fa-trash-alt"></i>
+                        </button>
+                    </form>
+
+                </td>
+            </tr>
+
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<div class="float-left mt-4">
+    <?= $pager->links('barang','paging') ?>
+</div>
+<script>
+    function edit(kode) {
+        window.location.href = ('/barang/edit/' + kode);
+    }
+
+    function hapus(kode) {
+        pesan = confirm('Yakin data barang ini dihapus ?');
+        if (pesan) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+</script>
+>>>>>>> Stashed changes
 <?= $this->endSection('isi') ?>
